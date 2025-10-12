feb086c93956321c6cd5ddecc14f0559c7669afc
CadenEnnis <cennis12@terpmail.umd.edu>
2025-10-12 13:13:03 -0400

    Save current version

--- project.py
+++ project.py
@@ -27,7 +27,40 @@ def calculate_area(length, width=None, shape='rectangle'):
         raise ValueError("The shape is not recognized.")
 #Caden Ennis
 
+def validate_container_dimensions(length, width, depth):
+    """
+    Validate that the container dimensions are reasonable sizes for the gardem.
+    
+    Parameters:
+        length(float): Length in inches
+        width(float): width in inches
+        depth(float): depth in inches
 
+    Returns:
+        bool: True if the dimensions are reasonable
+    
+    Raises: 
+        ValueError: if dimensions are not reasonable
+    """
+    #Checking if the dimensions are positive numbers
+    if length <= 0:
+        raise ValueError("Length must be positive.")
+    if width <= 0:
+        raise ValueError("Width must be positive.")
+    if depth <= 0:
+        raise ValueError("Depth must be positive.")
+    # Check maximum horizontal dimensions (20 feet = 240 inches)
+    if length > 240:
+        raise ValueError("Length is larger than maximum limit (240 inches).")
+    if width > 240:
+        raise ValueError("Width is larger than maximum limit (240 inches).")
+    #Check depth limits (min 2 inch, max 48 inch)
+    if depth < 2:
+        raise ValueError("Depth is smaller than minimum limit of 2 inches.")
+    if depth > 48:
+        raise ValueError("Depth is larger than maximum limit of 48 inches.")
+    return True
+#Caden Ennis
 
 
 
